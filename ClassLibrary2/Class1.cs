9096ac33c0313e777f4d5a5877d0803ca2b2d817
Welly Tambunan <if05041@gmail.com>
2016-09-05 15:45:06 +0700

    publish all unit test configuration

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleNetCoreUnitTests
+{
+    [TestClass]
+    public class TestClasss
+    {
+        [TestMethod]
+        public void TestMethodPassing()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethodFailing()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    [TestFixture]
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        [Test]
+        public void TestSuccess()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
